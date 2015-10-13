e28fcab0b5b02437f122128a7b087d0b5818701f
rtduany <rtduany@gmail.com>
2015-10-14 01:27:30 +0300

    modify field list and constructor

--- Teacher.java
+++ Teacher.java
@@ -1,14 +1,14 @@
 //define a Teacher class, subclass of Person class
 class Teacher extends Person{
 	//declare a couple of fields to provide for the state of the Teacher class and its objects
-	private int numCourses;
-	private String courses;
+	private int numCourses;		//number of courses taught currently
+	private String[] courses;	//corresponding course codes
+	private static final int MAX_COURSES = 10;	//maximum courses
 	//define 1 constructor that takes 5 parameters to initialize new objects of the Teacher class
-	public Teacher(int theNumCourses, String theCourses, String name, int yearOfBirth, String residence){
+	public Teacher(String name, int yearOfBirth, String residence){
 		//lets call the base class's constructor using the super() keyword
 		super(name, yearOfBirth, residence);
-		//set incoming arguments equal to the new object's instance variables
-		numCourses = theNumCourses;
-		courses = theCourses;
+		numCourses = 0;
+		courses = new String[MAX_COURSES];
 	}
 }
