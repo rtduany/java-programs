edb51256c61c9bc79e3537a7027c82f9c82f4d35
rtduany <rtduany@gmail.com>
2015-10-14 00:51:32 +0300

    add 1 constructor that takes 5 parameters to the Teacher class

--- Teacher.java
+++ Teacher.java
@@ -3,4 +3,12 @@ class Teacher extends Person{
 	//declare a couple of fields to provide for the state of the Teacher class and its objects
 	private int numCourses;
 	private String courses;
+	//define 1 constructor that takes 5 parameters to initialize new objects of the Teacher class
+	public Teacher(int theNumCourses, String theCourses, String name, int yearOfBirth, String residence){
+		//lets call the base class's constructor using the super() keyword
+		super(name, yearOfBirth, residence);
+		//set incoming arguments equal to the new object's instance variables
+		numCourses = theNumCourses;
+		courses = theCourses;
+	}
 }
