e9ed31948580e01bb45d2b2974c1cb0d208b910d
rtduany <rtduany@gmail.com>
2015-10-13 15:39:35 +0300

    add 3 fields to Person class

---
+++ Person.java
@@ -0,0 +1,9 @@
+/*write a java program that deines a class Person, a Student and Faculty classes that inherit from Person class. Then we define a Driver class to test our program*/
+
+//define a Person class
+class Person{
+	//lets declare 3 fields for the Person class and its objects
+	private String name;
+	private int yearOfBirth;
+	private String residence;
+}
