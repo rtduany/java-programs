b3171faa377ef6059ef391647d268fa85e06aae3
rtduany <rtduany@gmail.com>
2015-10-19 21:24:47 +0300

    fix program by changing salary type from String to double

--- Staff.java
+++ Staff.java
@@ -3,9 +3,9 @@ public class Staff extends Person{
 	//lets declare a 3 fields to provide for the state of the Staff class and its objects
 	private String title;
 	private int roomNumber;
-	private String salary;
+	private double salary;
 	//define 1 constructor that takes 6 parameters to initialize new objects for the Staff class
-	public Staff(String theTitle, int theRoomNumber, String theSalary, String name, int yearOfBirth, String residence){
+	public Staff(String theTitle, int theRoomNumber, double theSalary, String name, int yearOfBirth, String residence){
 		//lets call the base class's constructor using the keyword super()
 		super(name, yearOfBirth, residence);
 		//set incoming arguments equal to the new object's instance variables
@@ -14,13 +14,16 @@ public class Staff extends Person{
 		salary = theSalary;
 	}
 	//lets define 3 setter methods to provide for the unique behavior of the Staff class and its objects
+	//define a setTitle method to the title to the variable title
 	public void setTitle(String t){
 		title = t;
 	}
+	//define a setRoomNumber setter method to assign room number to the variable roomNumber
 	public void setRoomNumber(int roomNo){
 		roomNumber = roomNo;
 	}
-	public void setSalary(String s){
+	//define a setSalary setter method to assign the salary to the variable salary
+	public void setSalary(double s){
 		salary = s;
 	}
 	//define 3 getter methods to provide for the unique behavior of Staff class
@@ -33,7 +36,7 @@ public class Staff extends Person{
 		return roomNumber;
 	}
 	//define getSalary() method
-	public String getSalary(){
+	public double getSalary(){
 		return salary;
 	}
 }
