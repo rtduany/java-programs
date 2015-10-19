1ff86c0b0ab4c380fef9e8e1bd5423f6eddf0429
rtduany <rtduany@gmail.com>
2015-10-19 23:56:30 +0300

    modify program to make salary double

--- Teacher.java
+++ Teacher.java
@@ -3,10 +3,10 @@ class Teacher extends Person{
 	//declare a couple of fields to provide for the state of the Teacher class and its objects
 	private int numCourses;		//number of courses taught currently
 	private String[] courses;	//corresponding course codes
-	private String salary;
+	private double salary;
 	private static final int MAX_COURSES = 10;	//maximum courses
 	//define 1 constructor that takes 5 parameters to initialize new objects of the Teacher class
-	public Teacher(String salary, String name, int yearOfBirth, String residence){
+	public Teacher(double salary, String name, int yearOfBirth, String residence){
 		//lets call the base class's constructor using the super() keyword
 		super(name, yearOfBirth, residence);
 		//set incomin argument equal to instance variable
@@ -15,7 +15,7 @@ class Teacher extends Person{
 		courses = new String[MAX_COURSES];
 	}
 	//define 1 setter method to provide for the unique bahavior of the Teacher class and its objects
-	public void setSalary(String salary){
+	public void setSalary(double salary){
 		this.salary = salary;
 	}
 	//define 1 getter method to provide for the unique behavior of the Teacher class and its objects
